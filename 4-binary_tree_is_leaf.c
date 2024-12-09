7defba92fbad14dbe39e92bff2c95dc64237f007
aurelien <aurelienmest@outlook.fr>
2024-12-09 17:18:50 +0100

    chal8

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -2,23 +2,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * binary_tree_delete - Supprime un arbre binaire.
- * @tree: Pointeur vers la racine de l'arbre à supprimer.
+ * binary_tree_is_leaf - Checks if a node is a leaf node.
+ * @node: Pointer to the node to check.
  *
- * Cette fonction supprime récursivement tous les nœuds de l'arbre en
- * appelant la fonction sur les sous-arbres gauche et droit, puis
- * libère la mémoire allouée pour chaque nœud.
+ * Description: A leaf node is a node that has no .
+ *
+ * Return:
+ * - 1 if the node is a leaf (no children).
+ * - 0 if the node is not a leaf or if the node is NULL.
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-     if (node == NULL)
-    {
-        return (0);
-    }
-    else if(node->left == NULL && node->right == NULL)
-    {
-      
-        return(1);
-    }
-    return 0;
+if (node == NULL)
+{
+return (0);
+}
+else if (node->left == NULL && node->right == NULL)
+{
+return (1);
+}
+return (0);
 }

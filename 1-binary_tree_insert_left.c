c28f8a40f3d5a897701fa827f9d8897810d6aee5
aurelien <aurelienmest@outlook.fr>
2024-12-09 15:41:45 +0100

    chal8

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -2,28 +2,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * binary_tree_node - Crée un nouveau nœud dans un arbre binaire
- * @parent: Pointeur vers le parent du nouveau nœud
- * @value: Valeur à insérer dans le nouveau nœud
+ * binary_tree_insert_left - Insère un nœud comme enfant gauche d'un parent
+ * @parent: Pointeur vers le parent
+ * @value: Valeur à stocker dans le nouveau nœud
  *
- * Return: Pointeur vers le nouveau nœud, ou NULL en cas d'échec
+ * Return: Pointeur vers le nouveau nœud, ou NULL si une erreur se produit
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-
-    binary_tree_t *node;
-    if (parent == NULL)
-        return NULL;
+binary_tree_t *node;
 
 
-   node = binary_tree_node(parent, value);
+if (parent == NULL)
+return (NULL);
+node = binary_tree_node(parent, value);
 
-    if (parent->left != NULL) {
-        node->left = parent->left;
-        parent->left->parent = node;
-    }
+if (parent->left != NULL)
+{
+node->left = parent->left;
 
-    parent->left = node;
+parent->left->parent = node;
+}
 
-    return node;
+parent->left = node;
+
+return (node);
 }

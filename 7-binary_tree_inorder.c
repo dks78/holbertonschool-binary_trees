b6c104da7a506ce5afaa3502b126cb7046d950a9
aurelien <aurelienmest@outlook.fr>
2024-12-10 10:34:44 +0100

    chal8

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -7,15 +7,16 @@
  */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree == NULL || func == NULL)
-        return;
+if (tree == NULL || func == NULL)
+return;
 
-    /* Parcourir le sous-arbre gauche */
-    binary_tree_inorder(tree->left, func);
+/* Parcourir le sous-arbre gauche */
+binary_tree_inorder(tree->left, func);
 
-    /* Traiter le nœud courant */
-    func(tree->n);
+/* Traiter le nœud courant */
+func(tree->n);
 
-    /* Parcourir le sous-arbre droit */
-    binary_tree_inorder(tree->right, func);
+/* Parcourir le sous-arbre droit */
+binary_tree_inorder(tree->right, func);
 }
+

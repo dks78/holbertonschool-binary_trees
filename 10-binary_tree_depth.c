13d6aa507d54f75d92b769e88390619af210bd26
aurelien <aurelienmest@outlook.fr>
2024-12-10 14:35:50 +0100

    chal8

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,24 +1,25 @@
 #include "binary_trees.h"
 #include <stddef.h>
 /**
- * max - Returns the maximum value between two integers
- * @a: The first integer
- * @b: The second integer
+ * binary_tree_depth - Measures the depth of a binary tree.
+ * @tree: A pointer to the root node of the tree.
  *
- * Return: the larger of the two integers a and b.
+ * Return: The depth of the tree. If the tree is NULL, return 0.
  */
-size_t binary_tree_depth(const binary_tree_t *tree) {
-    size_t depth = 0;
-    const binary_tree_t *current = tree;
-    if(current == NULL)
-    {
-        return 0;
-    }
-    /*Remonter l'arbre jusqu'à la racine*/ 
-    while (current != NULL) {
-        current = current->parent; /*Remonte vers le parent*/ 
-        depth++; /*Incrémente la profondeur à chaque niveau*/ 
-    }
-
-    return depth -1;
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+size_t depth = 0;
+const binary_tree_t *current = tree;
+if (current == NULL)
+{
+return (0);
 }
+/*Remonter l'arbre jusqu'à la racine*/
+while (current != NULL)
+{
+current = current->parent; /*Remonte vers le parent*/
+depth++; /*Incrémente la profondeur à chaque niveau*/
+}
+return (depth (-1));
+}
+

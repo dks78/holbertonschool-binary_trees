199831f21ca6838de0ac9fb89b179742e9d76d4c
aurelien <aurelienmest@outlook.fr>
2024-12-10 11:37:39 +0100

    chal8

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -16,7 +16,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
     int RightHeight;
     if(tree == NULL)
     {
-        return 0;
+        return -1;
     }
 
     leftHeight = binary_tree_height(tree->left);

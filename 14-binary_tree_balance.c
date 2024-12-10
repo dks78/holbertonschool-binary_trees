a7d5bd5cfa68c1c44d61c99120cb4074ba5f9db8
aurelien <aurelienmest@outlook.fr>
2024-12-10 16:16:11 +0100

    chal8

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -2,40 +2,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * binary_tree_nodes - Counts the number of nodes with at least one child.
- * @tree: A pointer to the tree node of the tree to count the nodes.
+ * height - Helper function to calculate the height of a tree
+ * @tree: A pointer to the root node of the tree to calculate the height.
  *
- * Return: The number of nodes with at least one child.
- *         If tree is NULL, return 0.
+ * Return: The height of the tree, or 0 if the tree is NULL.
  */
 int height(const binary_tree_t *tree)
 {
-    int leftHeight;
-    int rightHeight;
-    if (tree == NULL)
-        return (0);
+int leftHeight;
+int rightHeight;
+if (tree == NULL)
+return (0);
 
-    /* Compute the height of the left and right subtrees */
-    leftHeight = height(tree->left);
-     rightHeight = height(tree->right);
-
-    /* Return the larger height plus 1 (for the current node) */
-    return (1 + (leftHeight > rightHeight ? leftHeight : rightHeight));
+/* Compute the height of the left and right subtrees */
+leftHeight = height(tree->left);
+rightHeight = height(tree->right);
+/* Return the larger height plus 1 (for the current node) */
+return (1 + (leftHeight > rightHeight ? leftHeight : rightHeight));
 }
-
-
-
+/**
+ * binary_tree_balance - Measures the balance factor of an AVL tree.
+ * @tree: A pointer to the root node of the tree to measure the balance factor.
+ *
+ * Return: The balance factor of the tree.
+ *         If tree is NULL, return 0.
+ */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-    int leftHeight;
-    int rightHeight;
-    if (tree == NULL)
-        return (0);
+int leftHeight;
+int rightHeight;
+if (tree == NULL)
+return (0);
 
-    /* Compute the height of the left and right subtrees */
-    leftHeight = height(tree->left);
-    rightHeight = height(tree->right);
+/* Compute the height of the left and right subtrees */
+leftHeight = height(tree->left);
+rightHeight = height(tree->right);
 
-    /* Return the balance factor (difference between left and right heights) */
-    return (leftHeight - rightHeight);
+/* Return the balance factor (difference between left and right heights) */
+return (leftHeight - rightHeight);
 }

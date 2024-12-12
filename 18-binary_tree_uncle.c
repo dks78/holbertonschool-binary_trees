6efec218a6545be6187571eab51038d5ebc1e8d3
aurelien <aurelienmest@outlook.fr>
2024-12-12 11:07:50 +0100

    chal8

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,18 +1,18 @@
 #include "binary_trees.h"
 /**
- * binary_tree_sibling - Finds the sibling of a node.
- * @node: Pointer to the node to find the sibling.
+ * binary_tree_uncle - Finds the uncle of a node.
+ * @node: Pointer to the node to find the uncle.
  *
- * Return: A pointer to the sibling node, or NULL if there is no sibling or
- *         if node or its parent is NULL.
+ * Return: A pointer to the uncle node, or NULL if there is no uncle,
+ *         or if node or its parent is NULL.
  */
- binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-   if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
-        return (NULL);
+if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+return (NULL);
 /* Si le nœud est à gauche, retourner le nœud à droite  */
-   if (node->parent == node->parent->parent->left)
-        return (node->parent->parent->right);
-    else
-        return (node->parent->parent->left);
+if (node->parent == node->parent->parent->left)
+return (node->parent->parent->right);
+else
+return (node->parent->parent->left);
 }

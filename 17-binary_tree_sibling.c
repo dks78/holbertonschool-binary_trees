71cf0920e087a773b09bb9fa59c1ad53ce5ce0a2
aurelien <aurelienmest@outlook.fr>
2024-12-12 11:02:39 +0100

    chal8

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,19 +1,18 @@
 #include "binary_trees.h"
 /**
- * binary_tree_is_full - Vérifie si un arbre binaire est complet
- * @tree: Pointeur vers la racine de l'arbre
+ * binary_tree_sibling - Finds the sibling of a node.
+ * @node: Pointer to the node to find the sibling.
  *
- * Return: 1 si l'arbre est complet, 0 sinon
+ * Return: A pointer to the sibling node, or NULL if there is no sibling or
+ *         if node or its parent is NULL.
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-
-    if (node == NULL || node->parent == NULL)
-        return (NULL);
-
-    /* Si le nœud est à gauche, retourner le nœud à droite (le frère) et vice versa */
-    if (node == node->parent->left)
-        return (node->parent->right);
-    else
-        return (node->parent->left);
+if (node == NULL || node->parent == NULL)
+return (NULL);
+/* Si le nœud est à gauche, retourner le nœud à droite  */
+if (node == node->parent->left)
+return (node->parent->right);
+else
+return (node->parent->left);
 }

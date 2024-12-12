1edbb33ef3dfb8ff20778c384169c413210cb755
aurelien <aurelienmest@outlook.fr>
2024-12-12 10:53:05 +0100

    chal8

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -6,37 +6,33 @@
  * max - Helper function to return the maximum of two integers.
  * @a: First integer.
  * @b: Second integer.
- * 
  * Return: The maximum of a and b.
  */
 int max(int a, int b)
 {
-    return ((a > b) ? a : b);
+return ((a > b) ? a : b);
 }
-
 /**
  * binary_tree_height - Measures the height of a binary tree.
  * @tree: Pointer to the root node of the tree.
  *
  * Return: the height of the binary tree. If the tree is NULL, return 0.
  */
-size_t binary_tree_height(const binary_tree_t *tree) /*ici c'est la fonction
-pour return a hauteur de l'abre */
+size_t binary_tree_height(const binary_tree_t *tree)
 {
-    int leftHeight;
-    int rightHeight;
+int leftHeight;
+int rightHeight;
 
-    if (tree == NULL)
-    {
-        return (0);
-    }
-
-    leftHeight = binary_tree_height(tree->left);
-    rightHeight = binary_tree_height(tree->right);
-
-    return (max(leftHeight, rightHeight) + 1);
+if (tree == NULL)
+{
+return (0);
 }
 
+leftHeight = binary_tree_height(tree->left);
+rightHeight = binary_tree_height(tree->right);
+
+return (max(leftHeight, rightHeight) + 1);
+}
 /**
  * binary_tree_depth - Measures the depth of a node in a binary tree.
  * @tree: Pointer to the node to measure the depth for.
@@ -45,21 +41,18 @@ pour return a hauteur de l'abre */
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {/*ici pour la profondeur */
-    size_t depth = 0;
-    const binary_tree_t *current = tree;
-    
-    while (current != NULL && current->parent != NULL)
-    {
-        current = current->parent;
-        depth++;
-    }
-
-    return depth;
+size_t depth = 0;
+const binary_tree_t *current = tree;
+while (current != NULL && current->parent != NULL)
+{
+current = current->parent;
+depth++;
 }
-
+return (depth);
+}
 /**
- * is_Perfect_recursive - Helper function to check if the binary tree is perfect.
- * @root: Pointer to the root node of the current subtree.
+ * is_Perfect_recursive - Helper function to check if the binarytreeisperfect.
+ * @tree: Pointer to the root node of the current subtree.
  * @depth: The depth of the tree.
  * @level: The current level (or depth) in the tree.
  *
@@ -67,26 +60,21 @@ size_t binary_tree_depth(const binary_tree_t *tree)
  */
 int is_Perfect_recursive(const binary_tree_t *tree, int depth, int level)
 {
-    if (tree == NULL)
-    {
-        return 1;
-    } /*Si tree est vide return 1 */
-
-    if (tree->left == NULL && tree->right == NULL)
-    {
-        return (depth == level + 1);
-    }
-
-    if (tree->left == NULL || tree->right == NULL)
-    {
-        return 0;
-    }
-
-
-    return is_Perfect_recursive(tree->left, depth, level + 1) &&
-           is_Perfect_recursive(tree->right, depth, level + 1);
+if (tree == NULL)
+{
+return  (1);
 }
-
+if (tree->left == NULL && tree->right == NULL)
+{
+return (depth == level + 1);
+}
+if (tree->left == NULL || tree->right == NULL)
+{
+return (0);
+}
+return (is_Perfect_recursive(tree->left, depth, level + 1)
+&& is_Perfect_recursive(tree->right, depth, level + 1));
+}
 /**
  * binary_tree_is_perfect - Checks if a binary tree is perfect.
  * @tree: Pointer to the root node of the tree to check.
@@ -95,15 +83,16 @@ int is_Perfect_recursive(const binary_tree_t *tree, int depth, int level)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-    int depth;
+int depth;
 
-    if (tree == NULL)
-    {
-        return 0;
-    }
-
-    depth = binary_tree_height(tree); 
-
-
-    return is_Perfect_recursive(tree, depth, 0);
+if (tree == NULL)
+{
+return (0);
 }
+
+depth = binary_tree_height(tree);
+
+
+return (is_Perfect_recursive(tree, depth, 0));
+}
+
